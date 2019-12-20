a5bec0cb979921a9e97cd3a3f557dbf222bd5874
Florin Buga <>
2019-12-20 16:46:06 +0200

    feat: get album data from the API

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AlbumService } from '../api/album.service';
+
 @Component({
   selector: 'app-list',
   templateUrl: 'list.page.html',
@@ -9,37 +11,12 @@ export class ListPage implements OnInit {
   private selectedItem: any;
 
   //public items: Array<{ title: string; note: string; icon: string }> = [];
-  public albums = [
-    {
-      "albumId": 1,
-      "id": 1,
-      "title": "accusamus beatae ad facilis cum similique qui sunt",
-      "url": "https://via.placeholder.com/600/92c952",
-      "thumbnailUrl": "https://via.placeholder.com/150/92c952"
-    },
-    {
-      "albumId": 1,
-      "id": 2,
-      "title": "reprehenderit est deserunt velit ipsam",
-      "url": "https://via.placeholder.com/600/771796",
-      "thumbnailUrl": "https://via.placeholder.com/150/771796"
-    },
-    {
-      "albumId": 1,
-      "id": 3,
-      "title": "officia porro iure quia iusto qui ipsa ut modi",
-      "url": "https://via.placeholder.com/600/24f355",
-      "thumbnailUrl": "https://via.placeholder.com/150/24f355"
-    },
-    {
-      "albumId": 1,
-      "id": 4,
-      "title": "culpa odio esse rerum omnis laboriosam voluptate repudiandae",
-      "url": "https://via.placeholder.com/600/d32776",
-      "thumbnailUrl": "https://via.placeholder.com/150/d32776"
-    }
-  ];
+  public albums: any[] = [];
+
+  constructor(private albumService: AlbumService) {}
 
   ngOnInit() {
+    this.albumService.getAlbums()
+      .subscribe((albums: any[]) => {this.albums = albums; console.log(albums);});
   }
 }
