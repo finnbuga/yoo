1dad64451b74557ea19f4ba7b66f2eee1304626a
Florin Buga <7334553+finnbuga@users.noreply.github.com>
2019-12-25 11:52:07 +0200

    style: apply Prettier formating to app.components.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,44 +1,44 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Platform } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  selector: "app-root",
+  templateUrl: "app.component.html",
+  styleUrls: ["app.component.scss"]
 })
 export class AppComponent {
   public appPages = [
     {
-      title: 'Dashboard',
-      url: '/dashboard',
-      icon: 'analytics',
+      title: "Dashboard",
+      url: "/dashboard",
+      icon: "analytics",
       isDisabled: true
     },
     {
-      title: 'Photos',
-      url: '/photos',
-      icon: 'images',
+      title: "Photos",
+      url: "/photos",
+      icon: "images",
       isDisabled: true
     },
     {
-      title: 'Albums',
-      url: '/albums',
-      icon: 'list',
+      title: "Albums",
+      url: "/albums",
+      icon: "list",
       isDisabled: false
     },
     {
-      title: 'My List',
-      url: '/my-list',
-      icon: 'list-box',
+      title: "My List",
+      url: "/my-list",
+      icon: "list-box",
       isDisabled: true
     },
     {
-      title: 'Chat',
-      url: '/chat',
-      icon: 'chatbubbles',
+      title: "Chat",
+      url: "/chat",
+      icon: "chatbubbles",
       isDisabled: true
     }
   ];
