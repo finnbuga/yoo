8bc6929f4779b6874af2af0d58ab15cc675bff71
Florin Buga <>
2019-12-26 20:19:46 +0200

    feat: populate Album Detail page with actual data

--- src/app/album/album-details/album-details.page.ts
+++ src/app/album/album-details/album-details.page.ts
@@ -1,10 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'
+
+import { IAlbum } from '../../shared/interfaces';
+import { AlbumService } from '../../api/album.service';
 
 @Component({
   selector: 'album-details',
   templateUrl: 'album-details.page.html'
 })
 export class AlbumDetailsPage implements OnInit {
+  private id: number = null;
+  public album: IAlbum = null;
+
+  constructor(private route: ActivatedRoute, private albumService: AlbumService) {}
+
   ngOnInit() {
+    this.route.params
+      .subscribe(params => this.id = params['id']);
+    this.albumService.getAlbum(this.id)
+      .subscribe((album: IAlbum) => this.album = album);
   }
 }
